3aae73c77eaaafd85217ecd879bf2cc7eb97d788
Mitrich-rus <146307143+Mitrich-rus@users.noreply.github.com>
2024-03-21 08:58:56 +1000

    working

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static string Prompt (string message)
+{
+Console.WriteLine(message);
+string input = Console.ReadLine();
+if (input == String.Empty)
+{
+Prompt("Введите строки для массива через пробел: ");
+}
+return input;
+}
+string input = Prompt("Введите строки для массива через пробел: ");
+Console.WriteLine(input);
+
+string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+for (int i = 0; i < words.Length; i++)
+{
+    if 
+Console.Write(words[i]);   
+}
+
