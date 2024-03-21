42ebbffe1966fc41c990e31be81081e4341b7759
Mitrich-rus <146307143+Mitrich-rus@users.noreply.github.com>
2024-03-21 10:35:45 +1000

    code

--- Program.cs
+++ Program.cs
@@ -1,20 +1,39 @@
-﻿static string Prompt (string message)
+﻿static string Prompt(string message)
 {
-Console.WriteLine(message);
-string input = Console.ReadLine();
-if (input == String.Empty)
-{
-Prompt("Введите строки для массива через пробел: ");
+    Console.WriteLine(message);
+    string input = Console.ReadLine();
+    if (input == String.Empty)
+    {
+        Prompt("Введите строки для массива через пробел: ");
+    }
+    return input;
 }
-return input;
-}
-string input = Prompt("Введите строки для массива через пробел: ");
-Console.WriteLine(input);
+
 
 string[] words = input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-for (int i = 0; i < words.Length; i++)
+
+static string[] resultArray(string[] words)
 {
-    if 
-Console.Write(words[i]);   
+    string[] tempArray = new string[words.Length];
+    int newLength = 0;
+    for (int i = 0; i < words.Length; i++)
+    {
+        if (words[i].Length < 4)
+        {
+
+            tempArray[newLength] = words[i];
+            newLength++;
+        }
+    }
+string[] resultArray = new string[newLength];
+for (int i = 0; i < newLength; i++)
+    {
+        resultArray[i] = tempArray[newLength];
+    }
+    return resultArray;
 }
 
+
+
+string input = Prompt("Введите строки для массива через пробел: ");
+Console.WriteLine(input);
